9ba574adb928c346d6112dfbde0baaa1d61f5b9b
Oleksandr Kozachek <62398296+Plutokim@users.noreply.github.com>
2025-12-03 23:45:54 +0200

    fix(backend): cors

--- backend/src/app/app.py
+++ backend/src/app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from ctx.ctx import Context
 
 
 def new_app(ctx: Context):
     app = Flask(__name__)
+    CORS(app)
 
     @app.route("/health")
     def health():
